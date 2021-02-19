f0617bb07e4646846a4f0a12a02171a0ada79a2f
Nicole Hoelzl <hoelzl.nici@gmail.com>
2021-02-19 15:29:22 +0100

    Change logging system from logzero to logging in DEBUG mode

--- code/main.py
+++ code/main.py
@@ -7,12 +7,16 @@ __author__ = "Your Name"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
-from logzero import logger
+import logging 
+
+# set log level to DEBUG
+logging.basicConfig(format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', 
+        level=logging.DEBUG)
 
 
 def main():
     """ Main entry point of the app """
-    logger.info("hello world")
+    logging.debug("hello world")
 
 
 if __name__ == "__main__":
